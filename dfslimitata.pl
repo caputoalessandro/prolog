735304bf312ca0c14e2fc3a9e39b22f8ce26ff85
acptIA <acpt1994@gmail.com>
2019-05-13 08:50:16 +0200

    Initial commit

---
+++ dfslimitata.pl
@@ -0,0 +1,28 @@
+
+:- use_module(library(ordsets)).
+:- ["blocchiord12.pl"].
+
+block(a).
+block(b).
+block(c).
+
+iniziale(S):- 
+    list_to_ord_set([ontable(a), ontable(b), on(c,b), clear(a),handempty], S).
+
+goal(G):- list_to_ord_set([on(c,a),ontable(a),ontable(b),clear(b)],G).
+
+finale(S):- goal(G), ord_subset(G,S).
+
+% dfs_aux(S,ListaAzioni,Visitati,Soglia)
+depth_limit_search(Soluzione,Soglia):-
+    iniziale(S),
+    dfs_aux(S,Soluzione,[S],Soglia).
+
+dfs_aux(S,[],_,_):-finale(S).
+dfs_aux(S,[Azione|AzioniTail],Visitati,Soglia):-
+    Soglia>0,
+    applicabile(Azione,S),
+    trasforma(Azione,S,SNuovo),
+    \+member(SNuovo,Visitati),
+    NuovaSoglia is Soglia-1,
+    dfs_aux(SNuovo,AzioniTail,[SNuovo|Visitati],NuovaSoglia).
