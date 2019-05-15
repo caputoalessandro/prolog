66e08126ca22fc50d2ea0f2f7ff7a6432796abac
heygent <heygent@users.noreply.github.com>
2019-05-15 16:31:44 +0200

    Versione funzionante ida_star

--- ida_star.pl
+++ ida_star.pl
@@ -1,38 +1,38 @@
 :- use_module(library(ordsets)).
 :- ["azioni.pl", "dominio.pl"].
 
-heuristic(_, 1).
+euristica(_, 1).
 
 ida_star(Soluzione) :-
     iniziale(S),
-    heuristic(S,SogliaMassima),
-    ida_star_aux(S,SogliaMassima,Soluzione).
+    euristica(S, SogliaMassima),
+    ida_star_aux(S, SogliaMassima, Soluzione).
 
-ida_star_aux(S, SogliaMassima,Soluzione) :-
+ida_star_aux(S, SogliaMassima, Soluzione) :-
     dfs_aux(S, Soluzione, [S], 0, SogliaMassima).
 
-ida_star_aux(S, _, Soluzione) :- 
-    findall(X,euristica_nuova_soglia(X),ListaEuristiche),
-    min_list(ListaEuristiche,Minimmo),
-    retract(euristica_nuova_soglia(EuristicaNuovaSoglia)),
-    ida_star_aux(S,Minimmo,Soluzione).
+ida_star_aux(S, _, Soluzione) :-
+    findall(X, euristica_nuova_soglia(X), ListaEuristiche),
+    min_list(ListaEuristiche, Minimmo),
+    retractall(euristica_nuova_soglia(_)),
+    ida_star_aux(S, Minimmo, Soluzione).
 
 dfs_aux(S, [], _, _, _) :-
     finale(S).
 
 dfs_aux(S, [Azione|AzioniTail], Visitati, SogliaAttuale, SogliaMassima) :-
-    SogliaAttuale =< SogliaMassima,
+    SogliaAttuale=<SogliaMassima,
     !,
     applicabile(Azione, S),
     trasforma(Azione, S, SNuovo),
     \+ member(SNuovo, Visitati),
-    NuovaSoglia is SogliaAttuale + 1,
+    NuovaSoglia is SogliaAttuale+1,
     dfs_aux(SNuovo, AzioniTail, [SNuovo|Visitati], NuovaSoglia, SogliaMassima).
 
 
 dfs_aux(S, _, _, SogliaAttuale, _) :-
-    heuristic(S, EuristicaDaStatoCorrente),
-    EuristicaNuovaSoglia is EuristicaDaStatoCorrente + SogliaAttuale,
+    euristica(S, EuristicaDaStatoCorrente),
+    EuristicaNuovaSoglia is EuristicaDaStatoCorrente+SogliaAttuale,
     assert(euristica_nuova_soglia(EuristicaNuovaSoglia)),
     false.
 
