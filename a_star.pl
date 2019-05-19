30b0e4c9098beebe5862af8ab06d456d94011369
heygent <heygent@users.noreply.github.com>
2019-05-19 17:02:45 +0200

    Rinomia bfs in a_star e riformatta

--- a_star.pl
+++ a_star.pl
@@ -1,41 +1,42 @@
 :- ["azioni.pl"].
 
 % euristica(+Stato,-ValoreEuristica)
-euristica(_,1).
+euristica(_, 1).
 
-bfs(Soluzione) :-
+a_star(Soluzione) :-
     iniziale(S),
-    bfs_aux([nodo(0,0,S, [])], [], Soluzione).
+    a_star_aux([nodo(0, 0, S, [])], [], Soluzione).
 
-% bfs_aux(Coda,Visitati,Soluzione)
-% Coda = [nodo(S,Azioni)|...]
-bfs_aux([nodo(_,_,S, Azioni)|_], _, Azioni) :-
+a_star_aux([nodo(_, _, S, Azioni)|_], _, Azioni) :-
     finale(S).
 
-bfs_aux([nodo(CostoPiuEuristica,Costo,S, Azioni)|Tail], Visitati, Soluzione) :-
+a_star_aux([nodo(CostoPiuEuristica, Costo, S, Azioni)|Tail], Visitati, Soluzione) :-
     findall(Applicabile, applicabile(Applicabile, S), ListaApplicabili),
-    genera_figli(nodo(CostoPiuEuristica,Costo,S, Azioni),
+    genera_figli(nodo(CostoPiuEuristica, Costo, S, Azioni),
                  ListaApplicabili,
                  [S|Visitati],
                  ListaFigli),
     append(Tail, ListaFigli, NuovaCoda),
     list_to_ord_set(NuovaCoda, ListaOrdinata),
-    bfs_aux(ListaOrdinata, [S|Visitati], Soluzione).
+    a_star_aux(ListaOrdinata, [S|Visitati], Soluzione).
 
+% genera_figli(+NodoPartenza, +AzioniApplicabili, +Visitati, -NuoviFigli)
 genera_figli(_, [], _, []).
 
-% genera_figli(+Nodopartenza, +AzioniApplicabili, +Visitati, - NuoviFigli )
 genera_figli(Nodo, [Applicabile|AltriApplicabili], Visitati, [Figlio|FigliTail]) :-
-    Nodo = nodo(_,Costo,S, AzioniPerS),
+    Nodo=nodo(_, Costo, S, AzioniPerS),
     trasforma(Applicabile, S, SNuovo),
     \+ member(SNuovo, Visitati),
     !,
-    euristica(SNuovo,Euristica),
-    costo(Applicabile,CostoApplicabile),
-    CostoFiglio is CostoApplicabile + Costo,
-    CostoPiuEuristicaFiglio is CostoFiglio + Euristica,
-    Figlio = nodo(CostoPiuEuristicaFiglio,CostoFiglio,SNuovo, [Applicabile|AzioniPerS]),
+    euristica(SNuovo, Euristica),
+    costo(Applicabile, CostoApplicabile),
+    CostoFiglio is CostoApplicabile+Costo,
+    CostoPiuEuristicaFiglio is CostoFiglio+Euristica,
+    Figlio=nodo(CostoPiuEuristicaFiglio, CostoFiglio, SNuovo, [Applicabile|AzioniPerS]),
     genera_figli(Nodo, AltriApplicabili, Visitati, FigliTail).
 
-genera_figli(nodo(CostoPiuEuristica,Costo,S, AzioniPerS), [_|AltriApplicabili], Visitati, FigliTail) :-
-    genera_figli(nodo(CostoPiuEuristica,Costo,S, AzioniPerS), AltriApplicabili, Visitati, FigliTail).
+genera_figli(nodo(CostoPiuEuristica, Costo, S, AzioniPerS), [_|AltriApplicabili], Visitati, FigliTail) :-
+    genera_figli(nodo(CostoPiuEuristica, Costo, S, AzioniPerS),
+                 AltriApplicabili,
+                 Visitati,
+                 FigliTail).
